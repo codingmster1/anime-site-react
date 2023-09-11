baa537813db4edc28cff6de0c4582a8fc891a2d0
Jeff <doylejeff98@gmail.com>
2023-09-11 17:30:28 -0400

    return const footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "App.css";
+import footerLogo from '../assets/skull.gif';
+
+const  Footer = () => {
+  return (
+    <>
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="footer__container">
+            <a href="/#nav">
+              <figure className="footer__logo--wrapper">
+                <img
+                  src={footerLogo}
+                  className="footer__logo"
+                  alt=""
+                />
+              </figure>
+            </a>
+            <div className="footer__link--list">
+              <div className="footer__link">
+                <a href="/#nav" className="footer__link--anchor link__hover">
+                  Home
+                </a>
+              </div>
+              <div className="footer__link">
+                <a href="/#search" className="footer__link--anchor link__hover">
+                  Search
+                </a>
+              </div>
+              <div className="footer__link">
+                <a
+                  href="/#trending"
+                  className="footer__link--anchor link__hover"
+                >
+                  Trending
+                </a>
+              </div>
+              <div className="footer__link">
+                <a href="#popular" className="footer__link--anchor link__hover">
+                  Popular
+                </a>
+              </div>
+            </div>
+            <div className="footer__copyright">
+              Copyright &copy; 2023 Jeff Doyle
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Footer;
