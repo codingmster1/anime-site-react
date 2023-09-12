1eecffe7de4c6b0b595902c4019ceb64dcf2622a
Jeff <doylejeff98@gmail.com>
2023-09-11 22:56:13 -0400

    no results jsx

---
+++ src/ui/NoRes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "App.css";
+
+function NoRes() {
+  return (
+    <div className="nores">
+      <p className="nores__para">
+        No results found! Check for spelling mistakes!
+      </p>
+    </div>
+  );
+}
+
+export default NoRes;
