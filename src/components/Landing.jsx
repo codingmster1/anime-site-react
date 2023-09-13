164c0a93fe85bcc700aa125dc24733f5a4d9cad6
Jeff <doylejeff98@gmail.com>
2023-09-13 00:57:54 -0400

    increased landing page results before search

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -17,7 +17,7 @@ function Landing({ searchRedirect }) {
   async function getData(option) {
     const variables = {
       page: 1,
-      perPage: 32,
+      perPage: 36, // bigger homepage selection
     };
 
 
